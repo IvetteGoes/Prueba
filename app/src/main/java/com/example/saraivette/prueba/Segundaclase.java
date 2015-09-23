7d9f473240eb67493f681f0661f5adbcb18d4478
IvetteGoes <ivette.tapatia@hotmail.com>
2015-09-23 00:48:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/saraivette/prueba/Segundaclase.java
@@ -0,0 +1,15 @@
+package com.example.saraivette.prueba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SaraIvette on 21/09/2015.
+ */
+public class Segundaclase extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla2);
+
+    }
+}
